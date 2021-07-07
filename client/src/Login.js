bf08af7643d5166ed25c35cb1d15bc88cd9b4246
Pedro Siqueira <johnpedrosiqueira@gmail.com>
2021-07-07 09:57:43 -0700

    [Login] Implement UI for login page

--- client/src/Login.js
+++ client/src/Login.js
@@ -10,6 +10,9 @@ import {
   TextField,
 } from "@material-ui/core";
 import { login } from "./store/utils/thunkCreators";
+import BgImage from "./assets/images/bg-img.png";
+import BubbleChat from "./assets/images/bubble.svg";
+import "./styles/signup-login.css";
 
 const Login = (props) => {
   const history = useHistory();
@@ -28,41 +31,69 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Button onClick={() => history.push("/register")}>Register</Button>
-        </Grid>
-        <form onSubmit={handleLogin}>
-          <Grid>
-            <Grid>
-              <FormControl margin="normal" required>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                />
-              </FormControl>
-            </Grid>
-            <FormControl margin="normal" required>
-              <TextField
-                label="password"
-                aria-label="password"
-                type="password"
-                name="password"
-              />
-            </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
+    <div className="login-page-container">
+      <div className="side-banner">
+        <div className="side-banner-description">
+          <img src={BubbleChat} alt="bubble-chat" width="67px" height="67px" />
+          <span>Converse with anyone with any language</span>
+        </div>
+        <img src={BgImage} alt="login-page" />
+      </div>
+      <div className="login-content-container">
+        <div className="login-header">
+          <Typography
+            style={{ color: "rgb(143, 143, 143)" }}
+            variant="subtitle2"
+          >
+            Don't have an account?
+          </Typography>
+          <Button
+            style={{ color: "#3a8dff", marginLeft: "100px" }}
+            className="login-signup-button"
+            onClick={() => history.push("/register")}
+          >
+            Register
+          </Button>
+        </div>
+        <Grid container justify="center">
+          <Box m={4}>
+            <form className="login-signup-form" onSubmit={handleLogin}>
+              <Grid>
+                <Typography variant="h5">Welcome back!</Typography>
+                <Grid>
+                  <FormControl margin="normal" required>
+                    <span className="form-label">Username</span>
+                    <TextField
+                      aria-label="username"
+                      style={{ width: "400px" }}
+                      name="username"
+                      type="text"
+                    />
+                  </FormControl>
+                </Grid>
+                <FormControl margin="normal" required>
+                  <span className="form-label">Password</span>
+                  <TextField
+                    aria-label="password"
+                    style={{ width: "400px" }}
+                    type="password"
+                    name="password"
+                  />
+                </FormControl>
+              </Grid>
+              <Button
+                class="submit-button"
+                type="submit"
+                variant="contained"
+                size="large"
+              >
                 Login
               </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Box>
-    </Grid>
+            </form>
+          </Box>
+        </Grid>
+      </div>
+    </div>
   );
 };
 
