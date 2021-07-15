1bc47f2273f30c2b49fea7a9b50539acdf40bae2
Pedro Siqueira <johnpedrosiqueira@gmail.com>
2021-07-15 02:25:29 -0700

    [SideBanner] make description dissapear if screen is too small

--- client/src/styles/sideBanner.js
+++ client/src/styles/sideBanner.js
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography, Hidden } from "@material-ui/core";
 import BgImage from "../assets/images/bg-img.png";
 import BubbleChat from "../assets/images/bubble.svg";
 
@@ -7,9 +7,12 @@ const SideBanner = ({ classes }) => (
     <Box className={classes.sideBannerDescription}>
       <img src={BubbleChat} alt="bubble-chat" width="67px" height="67px" />
       <Box mt={4}>
-        <Typography variant="h5">
-          Converse with anyone with any language
-        </Typography>{" "}
+        {/* hide the description below chat icon when screen size is too small */}
+        <Hidden smDown>
+          <Typography variant="h5">
+            Converse with anyone with any language
+          </Typography>{" "}
+        </Hidden>
       </Box>
     </Box>
     <img className={classes.bgImage} src={BgImage} alt="login-page" />
